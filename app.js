73296a44ec8a90882b5d4aabc7d598bcabd3eafd
Rohan R <rohanrathan21@gmail.com>
2021-04-07 02:22:49 +0530

    Initial setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+require('dotenv').config();
+
+// app.use('/api', require('./routes/index'));
+
+app.get('/', (req, res) => res.send(`Routes at /api`));
+
+app.use((req, res) => res.status(404).send(`404! Not Found!`));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
