893e26ae24539feff6f716458c82a9cfd5478ff9
Rohan R <rohanrathan21@gmail.com>
2021-04-07 17:57:56 +0530

    Connect sequelize to postgres

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const { Sequelize } = require('sequelize');
+
+module.exports = new Sequelize(
+    process.env.PGDATABASE,
+    process.env.PGUSER,
+    process.env.PGPASSWORD,
+    {
+        host: process.env.PGHOST,
+        dialect: 'postgres',
+    },
+);
