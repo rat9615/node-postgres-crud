057c77ba90e5b5d5f6dc5d13475f2494f9e655a3
Rohan R <rohanrathan21@gmail.com>
2021-04-07 15:42:28 +0530

    Configure body-parser

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 
 require('dotenv').config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use('/api', require('./routes/index'));
 
 app.get('/', (req, res) => res.send(`Routes at /api`));
