09babfd84b257c1353cee609f3bfbb116ab5350a
Rohan R <rohanrathan21@gmail.com>
2021-04-13 18:00:20 +0530

    Initialize models using sequelize cli

---
+++ models/index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+
+const basename = path.basename(__filename);
+const config = require('../config/config');
+
+const db = {};
+
+const sequelize = new Sequelize(
+    config.development.database,
+    config.development.username,
+    config.development.password,
+    {
+        host: config.development.host,
+        dialect: config.development.dialect
+    }
+);
+
+fs.readdirSync(__dirname)
+    .filter(
+        (file) =>
+            file.indexOf('.') !== 0 &&
+            file !== basename &&
+            file.slice(-3) === '.js'
+    )
+    .forEach((file) => {
+        const model = require(path.join(__dirname, file))(
+            sequelize,
+            Sequelize.DataTypes
+        );
+        db[model.name] = model;
+    });
+
+Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
---
+++ models/user.js
@@ -0,0 +1,32 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class User extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    User.init(
+        {
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            age: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            }
+        },
+        {
+            sequelize,
+            modelName: 'User',
+            tableName: 'User'
+        }
+    );
+    return User;
+};
